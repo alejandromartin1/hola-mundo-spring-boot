b3b5e3e680cc9e6000fd955a6dffb07bb8e32a69
AlejandroMartin <107871684+alejandromartin1@users.noreply.github.com>
2023-03-22 11:38:24 -0600

    Add files via upload

---
+++ holamundo_Alejandro Martin/src/test/java/pe/alex/holamundo/HolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.alex.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
